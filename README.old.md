f7157d34df8d0cc0432217bb9efe85e6af3ecebb
John U <arjorb@gmail.com>
2023-05-26 17:28:24 +0200

    initial installation of react app

---
+++ README.old.md
@@ -0,0 +1,2 @@
+# react-testing
+This repository contains a collection of tests designed to validate the functionality and behavior of a React application. The tests cover various aspects such as component rendering, user interactions and am using both jest and react testing library
--- index.js
+++
@@ -1,9 +0,0 @@
-const getFullName = (firstname, secondname) => {
-  return `${firstname} ${secondname}`;
-};
-const actualFullName = getFullname("John", "Uwimana");
-const expectedFullName = "John Uwimana";
-
-if (actualFullName !== expectedFullName) {
-  throw new Error(`${actualFullName} is not equal to ${expectedFullName}`);
-}
